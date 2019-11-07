e584be7209d5263fe69cfb05629fef4ddd78c443
Curtis Charles <ccharles2388@gmail.com>
2019-11-07 15:57:43 -0500

    updated with listening on port message

--- server.js
+++ server.js
@@ -21,4 +21,7 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
-app.listen(port);
+app.listen(port, function() {
+    console.log(`Listening On Port:${port}`)
+});
+
