cf5213316e06043954e51ab96b9cfdfa615bd6bd
Curtis Charles <ccharles2388@gmail.com>
2019-11-07 14:43:30 -0500

    created connection file

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+var mysql = require('mysql');
+
+
+module.exports = function()  {
+  return {
+    getMySQLConnection: function() {
+
+      var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root", //Your username
+        password: "", //Your password
+        database: "burgers_db"
+      });
+
+      connection.connect(function(err) {
+        if (err) throw err;
+      });
+
+      return connection;
+    }
+  }
+}
