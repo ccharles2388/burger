f58a75e077dcee2ad7643d0b009779a7ae4b999a
Curtis Charles <ccharles2388@gmail.com>
2019-11-07 14:03:34 -0500

    seeded database with data

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES('Bacon Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES('Mac and Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES('Vegan Truffle Burger', false);
