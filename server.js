3803e2acbe923475ff62c7261145e983226fa897
Curtis Charles <ccharles2388@gmail.com>
2019-11-07 20:09:41 -0500

    took out bodyParser

--- server.js
+++ server.js
@@ -1,27 +1,33 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
+const express = require("express");
+// const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-var app = express();
+const app = express();
 
+// Serve static content for the app from the "public"
+// directory in the application directory.
 app.use(express.static(process.cwd() + "/public"));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
+// Parse request body as JSON
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.use(methodOverride("_method"));
 
-var exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgers_controller.js");
+const routes = require("./controllers/burgers_controller.js");
 
-app.use("/", routes);
+app.use(routes);
 
-app.listen(port, function() {
-    console.log(`Listening On Port:${port}`)
-});
+app.listen(PORT, function() {
+    console.log(`SERVER LISTENING ON: http://localhost:${PORT}`);
+    console.log('----------------------------');
+  });
 
